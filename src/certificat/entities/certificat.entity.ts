10b2d0cc3b15439267bf70a72cb510f885521972
Yassine Ouabou <wayasyt@gmail.com>
2024-11-22 21:53:32 +0100

    relation institution

--- src/certificat/entities/certificat.entity.ts
+++ src/certificat/entities/certificat.entity.ts
@@ -1,13 +1,23 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { ImageCertificat } from "src/image-certificat/entities/image-certificat.entity";
+import { Institution } from "src/institution/entities/institution.entity";
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Certificat {
-    
+
     @PrimaryGeneratedColumn()
     id:number;
     @Column()
     nom:string;
     @Column()
     dateEmission:Date;
+
+    @ManyToOne(()=>Institution,institution=>institution.certificat)
+    @JoinColumn()
+    institution:Institution
+
+    @OneToOne(()=>ImageCertificat,imageCertificat=>imageCertificat.certificat)
+    @JoinColumn()
+    imageCertificat:ImageCertificat;
 
 }
